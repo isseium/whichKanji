a376ed2a0b090cb8dae31ca7ad1f30e20dc6c160
KOMATSU Issei <issei@mizuhataya.com>
2012-12-08 18:39:14 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhichKnaji::Application.config.secret_token = 'f2e40532b20f38d249a5f36cb74ba12f4bce96ec76fdef96ab8eacc7093535b93b6e8c1f1652cc73896eec9e978c52cb83ce9292dbe5fd7b719c29f090b96a2b'
